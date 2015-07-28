6c566ecc2dec9d6c3d843ed99364c867ebf14f9d
Arne Hilmann <arne@hilmann.de>
2015-07-28 17:07:40 +0200

    handle parsed events

--- lib/tccai/commroom.ex
+++ lib/tccai/commroom.ex
@@ -1,26 +1,42 @@
 defmodule TccAI.CommRoom do
   use GenServer
 
+
   def start_link(opts \\ []) do
     :gen_server.start_link({ :local, :commroom }, __MODULE__, [], opts)
     #:gen_server.start_link(__MODULE__, :ok, [])
   end
 
+
   def init(state \\ []) do
-    :io.format "starting commroom now~n"
+    :io.format "[commroom] starting...~n"
     { :ok, state }
   end
 
-  def handle_info({ :event, topic, data }, state) do
-    :io.format "incoming event: topic ~i, payload ~s~n", [topic, data]
+
+  defp handle_event(:unit_created, {unit_id, builder}) do
+    :io.format "[commroom] unit ~w created by ~w~n", [unit_id, builder]
+  end
+  defp handle_event(topic, data) do
+    :io.format "[commroom] unhandled topic ~w!~n", [topic]
+  end
+
+
+  def handle_info({ :event, topic, data }, state) when is_integer(topic) do
+    :io.format "[commroom] incoming raw event: topic ~w, payload ~s~n", [topic, data]
+    { :noreply, state }
+  end
+  def handle_info({ :event, topic, data }, state) when is_atom(topic) do
+    :io.format "[commroom] incoming parsed event: ~s, ~w~n", [topic, data]
+    handle_event(topic, data)
     { :noreply, state }
   end
   def handle_info(:tick, state) do
-    :io.format "tick~n"
+    :io.format "[commroom] tick~n"
     { :noreply, state }
   end
   def handle_info(others, state) do
-    :io.format "unknown event ~s, ignoring~n", [others]
+    :io.format "[commroom] unknown event ~s, ignoring~n", [others]
     { :noreply, state }
   end
 
