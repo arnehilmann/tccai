f2726cd308c1de3a1ee882294474f2b08db261b4
Arne Hilmann <arne@hilmann.de>
2017-07-07 15:55:59 +0200

    start tech tree handling

--- lib/tccai/units.ex
+++ lib/tccai/units.ex
@@ -1,7 +1,9 @@
 defmodule TccAI.Units do
   alias SpringRTS.Callbacks
+  alias SpringRTS.Callbacks.Resource
   alias SpringRTS.Callbacks.Unit
   alias SpringRTS.Callbacks.UnitDef
+  alias TccAI.Resources
 
   @moduledoc "aggregate unit info"
 
@@ -20,6 +22,21 @@ defmodule TccAI.Units do
         :category => to_string(UnitDef.get_category_string(unitdef_id)),
         :is_builder => to_string(UnitDef.is_builder(unitdef_id)),
       }
+    end
+  end
+
+  def fetch_unitdefs(max_size \\ 100) do
+    Enum.map Callbacks.get_unit_defs(max_size), fn id ->
+      result = %{
+        :id => id,
+        :human_name => UnitDef.get_human_name(id),
+        :can_build => UnitDef.get_build_options(id),
+      }
+      costs = Enum.map Resources.valid_resource_ids(), fn r_id ->
+        %{Resource.get_name(r_id) => UnitDef.get_cost(id, r_id)}
+      end
+
+      Enum.reduce(costs, result, fn (map, acc) -> Map.merge(acc, map) end)
     end
   end
 end
