666b9c9c9449089fd08df65e49188919b965d003
Arne Hilmann <arne@hilmann.de>
2017-07-07 11:02:39 +0200

    use sub-modules of callbacks module

--- lib/tccai/units.ex
+++ lib/tccai/units.ex
@@ -1,21 +1,25 @@
 defmodule TccAI.Units do
-  import SpringRTS.Callbacks, only: :functions
+  alias SpringRTS.Callbacks
+  alias SpringRTS.Callbacks.Unit
+  alias SpringRTS.Callbacks.UnitDef
 
   @moduledoc "aggregate unit info"
 
   def overview do
-    Enum.map get_team_units(), fn id ->
-      unitdef_id = SpringRTS.Callbacks.Unit.get_def(id)
+    Enum.map Callbacks.get_team_units(), fn id ->
+      unitdef_id = Unit.get_def(id)
       %{
         :id => to_string(id),
         :def_id => to_string(unitdef_id),
-        :name => to_string(SpringRTS.Callbacks.UnitDef.get_name(unitdef_id)),
-#        :health => unit_getHealth(id),
-#        :max_health => unit_getMaxHealth(id),
-#        :isBeingBuilt => unit_isBeingBuilt(id),
-        # :pos => unit_getPos(id)
+        :name => to_string(UnitDef.get_name(unitdef_id)),
+        :human_name => to_string(UnitDef.get_human_name(unitdef_id)),
+        :health => Unit.get_health(id),
+        :max_health => Unit.get_max_health(id),
+        :is_being_built => Unit.is_being_built(id),
+        :pos => Unit.get_pos(id),
+        :category => UnitDef.get_category_string(unitdef_id),
+        :is_builder => UnitDef.is_builder(unitdef_id),
       }
     end
   end
-
 end
